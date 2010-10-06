bc77eeca185a6191a02b82e2f7e8c82efa022d33
ichverstehe <harry@vangberg.name>
2010-10-06 03:51:27 +0200

    refactor tests

--- lib/couchchanges.rb
+++ lib/couchchanges.rb
@@ -20,67 +20,56 @@ class CouchChanges
     block ? @disconnect = block : @disconnect
   end
 
-  def listen options={}
-    listener = Listener.new(self, options)
-    listener.start
-    listener
+  def initialize options={}
+    @url = options.delete(:url)
+    @options = options
+    @last_seq = 0
   end
 
-  class Listener
-    def initialize changes, options
-      @changes  = changes
-      @url      = options.delete(:url)
-      @options  = options
-      @last_seq = 0
-    end
-
-    attr_reader :http
-
-    def start
-      @http = http!
-      buffer = ""
-      @http.stream  {|chunk|
-        buffer += chunk
-        while line = buffer.slice!(/.+\r?\n/)
-          handle line
-        end
-      }
-      @http
-    end
-
-    def reconnect_since last_seq
-      @options[:since] = last_seq
-      start
-    end
-
-    def handle line
-      return if line.chomp.empty?
-
-      hash = JSON.parse(line)
-      if hash["last_seq"]
-        @changes.disconnect.call hash["last_seq"]
-        #reconnect_since hash["last_seq"]
-      else
-        hash["rev"] = hash.delete("changes")[0]["rev"]
-        callbacks hash
+  def listen
+    @http = http!
+    buffer = ""
+    @http.stream  {|chunk|
+      buffer += chunk
+      while line = buffer.slice!(/.+\r?\n/)
+        handle line
       end
-    end
+    }
+    @http
+  end
 
-    def callbacks hash
-      @changes.change.call hash if @changes.change
-      if hash["deleted"]
-        @changes.delete.call hash if @changes.delete
-      else
-        @changes.update.call hash if @changes.update
-      end
-    end
+  def http!
+    url = @url + "/_changes"
+    EM::HttpRequest.new(url).get(
+      :timeout => 0,
+      :query   => @options.merge({:feed => "continuous"})
+    )
+  end
 
-    def http!
-      url = @url + "/_changes"
-      EM::HttpRequest.new(url).get(
-        :timeout => 0,
-        :query   => @options.merge({:feed => "continuous"})
-      )
+  def reconnect_since last_seq
+    @options[:since] = last_seq
+    start
+  end
+
+  def handle line
+    return if line.chomp.empty?
+
+    hash = JSON.parse(line)
+    if hash["last_seq"]
+      @disconnect.call hash["last_seq"]
+      #reconnect_since hash["last_seq"]
+    else
+      hash["rev"] = hash.delete("changes")[0]["rev"]
+      callbacks hash
+    end
+  end
+
+  def callbacks hash
+    @change.call hash if @change
+    if hash["deleted"]
+      @delete.call hash if @delete
+    else
+      @update.call hash if @update
     end
   end
 end
--- test/test_changes.rb
+++ test/test_changes.rb
@@ -12,11 +12,12 @@ DATABASE = "couchchanges-test"
 URL      = "#{COUCH}/#{DATABASE}"
 
 class TestCouchChanges < Test::Unit::TestCase
-  setup do
-    db.recreate!
-    @changes = CouchChanges.new
-    @doc     = {"foo" => "bar"}
-    db.save_doc @doc
+  def changes options={}
+    EM.run {
+      c = CouchChanges.new({:url => URL}.merge(options))
+      yield c
+      c.listen
+    }
   end
 
   def couch
@@ -27,198 +28,159 @@ class TestCouchChanges < Test::Unit::TestCase
     @db ||= couch.database(DATABASE)
   end
 
-  def listen! args={}
-    args = {:url => URL}.merge(args)
-    @changes.listen(args)
+  setup do
+    db.recreate!
+    @doc = {"foo" => "bar"}
+    db.save_doc @doc
   end
 
   test "new document" do
-    EM.run {
-      @changes.change {|c|
+    changes do |c|
+      c.change {|c|
         assert_equal @doc["_id"],  c["id"]
         assert_equal @doc["_rev"], c["rev"]
         EM.stop
       }
 
-      @changes.update {|c|
+      c.update {|c|
         assert_equal @doc["_id"],  c["id"]
         assert_equal @doc["_rev"], c["rev"]
         EM.stop
       }
 
-      @changes.delete {|c| flunk "delete triggered"; EM.stop}
-
-      listen!
-    }
+      c.delete {|c| flunk "delete triggered"; EM.stop}
+    end
   end
 
   test "update document" do
-    EM.run {
-      @doc["new_key"] = "new value"
-      db.save_doc @doc
+    @doc["new_key"] = "new value"
+    db.save_doc @doc
 
-      @changes.update {|c|
+    changes do |c|
+      c.update {|c|
         assert_equal @doc["_id"],  c["id"]
         assert_equal @doc["_rev"], c["rev"]
         EM.stop
       }
 
-      @changes.delete {|c| flunk "delete triggered"; EM.stop}
-
-      listen!
-    }
+      c.delete {|c| flunk "delete triggered"; EM.stop}
+    end
   end
 
   test "delete document" do
-    EM.run {
-      db.delete_doc @doc
+    db.delete_doc @doc
 
-      @changes.delete {|c|
+    changes do |c|
+      c.delete {|c|
         assert c["deleted"]
         assert_equal @doc["_id"],  c["id"]
         EM.stop
       }
 
-      @changes.update {|c| flunk "update triggered"; EM.stop}
-
-      listen!
-    }
+      c.update {|c| flunk "update triggered"; EM.stop}
+    end
   end
 
   test "line spanning multiple chunks (\\n)" do
     EM.run {
-      @changes.delete {|c|
+      c = CouchChanges.new(:url => URL)
+
+      c.delete {|c|
         assert_equal "doc1", c["id"]
         EM.stop
       }
-      listener = listen!
+      
+      listener = c.listen
 
-      listener.http.on_decoded_body_data "{\"seq\":129,\"i"
-      listener.http.on_decoded_body_data "d\":\"doc1\",\"changes\":[{\"rev\":\"6-9ed852183290a143552caf4df76dea87\"}],\"deleted\":true}\n"
+      listener.on_decoded_body_data "{\"seq\":129,\"i"
+      listener.on_decoded_body_data "d\":\"doc1\",\"changes\":[{\"rev\":\"6-9ed852183290a143552caf4df76dea87\"}],\"deleted\":true}\n"
     }
   end
 
-  #test "multiple changes" do
-    #EM.run {
-      #doc2, doc3 = {}, {"doc"=>3}
-      #db.save_doc doc2
-      #db.save_doc doc3
-      #db.delete_doc doc3
+  test "multiple changes" do
+    doc2, doc3 = {}, {"doc" => 3}
+    db.save_doc doc2
+    db.save_doc doc3
+    db.delete_doc doc3
 
-      #count = 0
+    count = 0
 
-      #@changes.change {|c| count += 1 }
-      #@changes.delete {|c|
-        #assert_equal 3, count
-        #EM.stop
-      #}
-
-      #listen!
-    #}
-  #end
-
-  test "listen to multiple feeds" do
-    EM.run {
-      db2 = couch.database(DATABASE + "2")
-      db2.recreate!
-
-      doc2 = {}
-      db2.save_doc doc2
-      db2.delete_doc doc2
-
-      changed = []
-
-      @changes.change {|c| changed << c["id"]}
-      @changes.delete {|c|
-        assert_equal 2, changed.size
-        assert changed.include?(@doc["_id"]),
-          "#{changed} doesn't include #{@doc["_id"]}"
-        assert changed.include?(doc2["_id"]),
-          "#{changed} doesn't include #{doc2["_id"]}"
+    changes do |c|
+      c.change {|c| count += 1 }
+      c.delete {|c|
+        assert_equal 3, count
         EM.stop
       }
-
-      @changes.listen :url => URL
-      @changes.listen :url => (URL + "2")
-    }
+    end
   end
 
   test "since" do
-    EM.run {
-      seq  = db.info["update_seq"]
-      doc2 = {}
-      db.save_doc doc2
+    seq = db.info["update_seq"]
+    doc2 = {}
+    db.save_doc doc2
 
-      @changes.change {|c|
+    changes :since => seq do |c|
+      c.change {|c|
         assert c["rev"] > @doc["_rev"], "earlier revision"
         assert_equal doc2["_id"], c["id"]
         EM.stop
       }
-
-      listen! :since => seq
-    }
+    end
   end
 
   test "include_docs" do
-    EM.run {
-      @changes.update {|c|
+    changes :include_docs => true do |c|
+      c.update {|c|
         assert_not_nil c["doc"]
         assert_equal @doc["_id"],  c["doc"]["_id"]
         assert_equal @doc["_rev"], c["doc"]["_rev"]
         assert_equal @doc["foo"],  c["doc"]["foo"]
         EM.stop
       }
-
-      listen! :include_docs => true
-    }
+    end
   end
 
   test "filter" do
-    EM.run {
-      db.save_doc({
-        "_id"     => "_design/app",
-        "filters" => {
-          "filtered" => "function(doc, req) { return doc.type === 'filtered' }"
-        }
-      })
+    db.save_doc({
+      "_id"     => "_design/app",
+      "filters" => {
+        "filtered" => "function(doc, req) { return doc.type === 'filtered' }"
+      }
+    })
 
-      filtered = {"type" => "filtered"}
-      other    = {"type" => "other"}
-      db.save_doc filtered
-      db.save_doc other
+    filtered = {"type" => "filtered"}
+    other    = {"type" => "other"}
+    db.save_doc filtered
+    db.save_doc other
 
-      @changes.update {|c|
+    changes :filter => "app/filtered" do |c|
+
+      c.update {|c|
         assert_equal filtered["_id"], c["id"]
         EM.stop
       }
-
-      listen! :filter => "app/filtered"
-    }
+    end
   end
 
   test "heartbeat" do
-    EM.run {
-      @changes.change {|c|
+    changes :heartbeat => 100 do |c|
+      c.change {|c|
         assert_equal @doc["_id"],  c["id"]
         assert_equal @doc["_rev"], c["rev"]
       }
 
       EM.add_timer(0.2) { EM.stop }
-
-      listen! :heartbeat => 100
-    }
+    end
   end
 
   test "with disconnect: invoke with last_seq" do
-    EM.run {
-      @changes.disconnect {|last_seq|
+    changes :timeout => 1 do |c|
+      c.disconnect {|last_seq|
         assert_equal 1, last_seq
         EM.stop
       }
       EM.add_timer(0.2) { flunk "should invoke disconnect handler" }
-
-      listen! :timeout => 1
-    }
+    end
   end
 
   #test "without disconnect: default to reconnect" do
